8baa368cf951f7fa804209485336ed5412937f1c
János Orcsik <orcsikj@gmail.com>
2023-07-17 20:33:04 +0200

    works

---
+++ src/app/articles/page.tsx
@@ -0,0 +1,38 @@
+import { getAllPostData } from '@/lib/blogUtils'
+import Link from 'next/link'
+
+export default async function Page() {
+  const datas = await getAllPostData()
+
+  return (
+    <ul role="list" className="divide-y divide-gray-100">
+      {datas.map(({ slug, title, author }) => {
+        return (
+          <li
+            key={slug}
+            className="flex items-center justify-between gap-x-6 py-5"
+          >
+            <div className="min-w-0">
+              <div className="flex items-start gap-x-3">
+                <p className="text-sm font-semibold text-white">{title}</p>
+              </div>
+              <div className="mt-1 flex items-center gap-x-2 text-xs text-gray-500">
+                <p className="truncate">
+                  {author ? `Written by ${author}` : ''}
+                </p>
+              </div>
+            </div>
+            <div className="flex flex-none items-center gap-x-4">
+              <Link
+                href={{ pathname: `/articles/${slug}` }}
+                className="hidden rounded-md bg-gray-900 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-white sm:block hover:text-gray-900"
+              >
+                View post<span className="sr-only">, {title}</span>
+              </Link>
+            </div>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
---
+++ src/app/menu.tsx
@@ -0,0 +1,77 @@
+'use client'
+
+import { Dialog } from '@headlessui/react'
+import { Bars3Icon } from '@heroicons/react/20/solid'
+import { XMarkIcon as XMarkIconOutline } from '@heroicons/react/24/outline'
+import Link from 'next/link'
+import { useState } from 'react'
+
+const links = [
+  { text: 'Home', href: '/' },
+  { text: 'Articles', href: '/articles' },
+]
+
+export default function Menu() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
+  return (
+    <header className="flex h-16 border-b border-gray-900/10">
+      <div className="mx-auto flex w-full max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
+        <div className="flex flex-1 items-center gap-x-6">
+          <button
+            type="button"
+            className="-m-3 p-3 md:hidden"
+            onClick={() => setMobileMenuOpen(true)}
+          >
+            <span className="sr-only">Open main menu</span>
+            <Bars3Icon className="h-5 w-5 text-white" aria-hidden="true" />
+          </button>
+        </div>
+        <nav className="hidden md:flex md:gap-x-11 md:text-sm md:font-semibold md:text-white">
+          {links.map(({ text, href }) => {
+            return (
+              <Link key={text} href={{ pathname: href }}>
+                {text}
+              </Link>
+            )
+          })}
+        </nav>
+        <div className="flex flex-1 items-center justify-end gap-x-8"></div>
+      </div>
+      <Dialog
+        as="div"
+        className="lg:hidden"
+        open={mobileMenuOpen}
+        onClose={setMobileMenuOpen}
+      >
+        <div className="fixed inset-0 z-50" />
+        <Dialog.Panel className="fixed inset-y-0 left-0 z-50 w-full overflow-y-auto bg-gray-800 px-4 pb-6 sm:max-w-sm sm:px-6 sm:ring-1 sm:ring-gray-900/10">
+          <div className="-ml-0.5 flex h-16 items-center gap-x-6">
+            <button
+              type="button"
+              className="-m-2.5 p-2.5 text-white"
+              onClick={() => setMobileMenuOpen(false)}
+            >
+              <span className="sr-only">Close menu</span>
+              <XMarkIconOutline className="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div className="mt-2 space-y-2">
+            {links.map(({ text, href }) => {
+              return (
+                <Link
+                  key={text}
+                  href={{ pathname: href }}
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold text-white hover:text-gray-800 hover:bg-white"
+                  onClick={() => setMobileMenuOpen(false)}
+                >
+                  {text}
+                </Link>
+              )
+            })}
+          </div>
+        </Dialog.Panel>
+      </Dialog>
+    </header>
+  )
+}
